fd45c27b1c14caaaad80d767814ac078da7548c5
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2008-10-14 12:37:26 +0000

    Adding prefix and preview data from Publication table

--- fw-rest-api/src/main/java/com/fatwire/cs/rest/model/Publication.java
+++ fw-rest-api/src/main/java/com/fatwire/cs/rest/model/Publication.java
@@ -7,6 +7,14 @@ public class Publication {
 
     private String description;
 
+    private String pubroot;
+
+    private String preview;
+
+    private String prefix;
+
+    private String previewasset;
+
     public Publication() {
         super();
     }
@@ -53,17 +61,81 @@ public class Publication {
         this.name = name;
     }
 
+    /**
+     * @return the pubroot
+     */
+    public String getPubroot() {
+        return pubroot;
+    }
+
+    /**
+     * @param pubroot the pubroot to set
+     */
+    public void setPubroot(String pubroot) {
+        this.pubroot = pubroot;
+    }
+
+
+    /**
+     * @return the preview
+     */
+    public String getPreview() {
+        return preview;
+    }
+
+    /**
+     * @param preview the preview to set
+     */
+    public void setPreview(String preview) {
+        this.preview = preview;
+    }
+
+    /**
+     * @return the prefix
+     */
+    public String getPrefix() {
+        return prefix;
+    }
+
+    /**
+     * @param prefix the prefix to set
+     */
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    /**
+     * @return the previewasset
+     */
+    public String getPreviewAsset() {
+        return previewasset;
+    }
+
+    /**
+     * @param previewasset the previewasset to set
+     */
+    public void setPreviewAsset(String previewasset) {
+        this.previewasset = previewasset;
+    }
+
     /* (non-Javadoc)
      * @see java.lang.Object#hashCode()
      */
     @Override
     public int hashCode() {
-        final int PRIME = 31;
+        final int prime = 31;
         int result = 1;
-        result = PRIME * result
+        result = prime * result
+                + ((prefix == null) ? 0 : prefix.hashCode());
+        result = prime * result
+                + ((preview == null) ? 0 : preview.hashCode());
+        result = prime * result
+                + ((previewasset == null) ? 0 : previewasset.hashCode());
+        result = prime * result
                 + ((description == null) ? 0 : description.hashCode());
-        result = PRIME * result + (int) (id ^ (id >>> 32));
-        result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((pubroot == null) ? 0 : pubroot.hashCode());
         return result;
     }
 
@@ -72,25 +144,61 @@ public class Publication {
      */
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (!(obj instanceof Publication)) {
             return false;
-        final Publication other = (Publication) obj;
+        }
+        Publication other = (Publication) obj;
+        if (prefix == null) {
+            if (other.prefix != null) {
+                return false;
+            }
+        } else if (!prefix.equals(other.prefix)) {
+            return false;
+        }
+        if (preview == null) {
+            if (other.preview != null) {
+                return false;
+            }
+        } else if (!preview.equals(other.preview)) {
+            return false;
+        }
+        if (previewasset == null) {
+            if (other.previewasset != null) {
+                return false;
+            }
+        } else if (!previewasset.equals(other.previewasset)) {
+            return false;
+        }
         if (description == null) {
-            if (other.description != null)
+            if (other.description != null) {
                 return false;
-        } else if (!description.equals(other.description))
+            }
+        } else if (!description.equals(other.description)) {
             return false;
-        if (id != other.id)
+        }
+        if (id != other.id) {
             return false;
+        }
         if (name == null) {
-            if (other.name != null)
+            if (other.name != null) {
                 return false;
-        } else if (!name.equals(other.name))
+            }
+        } else if (!name.equals(other.name)) {
             return false;
+        }
+        if (pubroot == null) {
+            if (other.pubroot != null) {
+                return false;
+            }
+        } else if (!pubroot.equals(other.pubroot)) {
+            return false;
+        }
         return true;
     }
 
